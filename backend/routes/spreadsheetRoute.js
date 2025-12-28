bec9a98bfd322ded233fbd7054bfeb76fa1825f1
lukatexier <luka@famille-texier.fr>
2025-12-28 17:03:04 +0100

    Ajout des routes pour le spreadsheet

---
+++ backend/routes/spreadsheetRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const spreadsheetController = require('../controller/spreadsheetController');
+
+// Récupérer la liste des étudiants
+router.get('/etudiants', spreadsheetController.getUsers);
+
+// Récupérer l'EDT d'un étudiant
+router.get('/etudiants/:id', spreadsheetController.getEDT);
+
+// Modifier un cours d'un étudiant
+router.post('/etudiants/:id', spreadsheetController.setCours);
+
+module.exports = router;
