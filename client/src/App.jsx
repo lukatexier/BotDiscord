7d5fc36b720680faa4e2cf60bc45f39e73f1179d
lukatexier <luka@famille-texier.fr>
2025-12-29 11:26:43 +0100

    mise en place de l'environnemnt react

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Home from './pages/home';
+import Etudiants from './pages/etudiants';
+import Edt from './pages/edt';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/etudiants" element={<Etudiants />} />
+        <Route path="/edt/:id" element={<Edt />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
