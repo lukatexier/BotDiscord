86d7e4e929b752ec5ac8e9d7f20df8ac8b03c1ea
lukatexier <luka@famille-texier.fr>
2025-12-29 11:29:12 +0100

    ajout page pour la page de garde

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Bienvenue sur le gestionnaire d'emploi du temps</h1>
+      <p>Ceci est votre page d'accueil.</p>
+    </div>
+  );
+}
+
+export default Home;
